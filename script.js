f8fa1f101880eed0caa820f26794dc81d7442efe
jaidaken <johndoe@example.comjamiehewitt@protonmail.com>
2022-12-02 15:13:00 +0000

    placehold correct

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const addBtn = document.getElementById("addBtn");
 const newTodo = document.getElementById("newItem");
 const list = document.querySelector("ul");
 
-const checkbox = document.querySelector('.checkbox');
+const checkbox = document.querySelector(".checkbox");
 const tick = document.getElementById("tick");
 
 addBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
   if (document.getElementById("placehold")) {
-    list.innerHTML += `<li> <button id="checkbox" class="checkbox incomplete"> <img id="tick" src="./images/icon-check.svg" alt="tick"> </button> <p>${newTodo.value}</p> </li>`;
+    list.innerHTML = `<li> <button id="checkbox" class="checkbox incomplete"> <img id="tick" src="./images/icon-check.svg" alt="tick"> </button> <p>${newTodo.value}</p> </li>`;
     newTodo.value = "";
   } else {
     list.innerHTML += `<li> <button id="checkbox" class="checkbox incomplete"> <img id="tick" src="./images/icon-check.svg" alt="tick"> </button> <p>${newTodo.value}</p> </li>`;
@@ -18,5 +18,5 @@ addBtn.addEventListener("click", (event) => {
 });
 
 checkbox.addEventListener("click", () => {
-	console.log('clicked')
-})
+  console.log("clicked");
+});
